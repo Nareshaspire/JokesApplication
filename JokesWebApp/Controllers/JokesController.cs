412f119491f36a593f20775e2fbb07dc0f0c5769
nareshaspire <Nareshaspire45@gmail.com>
2022-12-05 21:10:18 -0500

    all changes

--- JokesWebApp/Controllers/JokesController.cs
+++ JokesWebApp/Controllers/JokesController.cs
@@ -64,8 +64,7 @@ namespace JokesWebApp.Controllers
         }
 
         // POST: Jokes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -98,8 +97,7 @@ namespace JokesWebApp.Controllers
         }
 
         // POST: Jokes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+     
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
