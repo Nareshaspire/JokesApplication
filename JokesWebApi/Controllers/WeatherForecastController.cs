15971891fafc877afe1126c7cdf0470f9814a353
nareshaspire <Nareshaspire45@gmail.com>
2022-12-08 23:12:19 -0500

    apis,tests

--- JokesWebApi/Controllers/WeatherForecastController.cs
+++ JokesWebApi/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace JokesWebApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[JokesWebApp]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
--- JokesWebApi/Program.cs
+++ JokesWebApi/Program.cs
@@ -1,7 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +15,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
@@ -23,3 +25,30 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+/*using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();*/
